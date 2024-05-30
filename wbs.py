7cdc27be27e687d3dce1bb596d11c7b3dbdfac9f
BahareTabibi <vernal.tb@gmail.com>
2024-05-31 02:47:52 +0330

    first commit

---
+++ wbs.py
@@ -0,0 +1,25 @@
+import requests
+
+URL = "https://unsplash.com/"
+
+#function to connect -----------------------------------------------------------------------
+def send_req(url):
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("connected succesfully.")
+    
+    else:
+        print("connection failed.")
+
+
+#getting photos ----------------------------------------------------------------------------
+def get_photos(url):
+    response = send_req(url)
+    if response:
+        print(response.text)
+
+    else:
+        return[]
+    
+get_photos(URL)
