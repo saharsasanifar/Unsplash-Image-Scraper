915fd01e7e63984b1b4bd17878228457aef4d3b6
saharsasanifar <sasanifarsahar@gmail.com>
2024-06-27 12:33:33 +0330

    adding functions for webscrapping

--- wbs.py
+++ wbs.py
@@ -1,6 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
+import os
 
 URL = "https://unsplash.com/"
+BASE_DIR = "downloaded_images"
 
 #function to connect -----------------------------------------------------------------------
 def send_req(url):
@@ -8,18 +11,64 @@ def send_req(url):
     response = requests.get(url)
     if response.status_code == 200:
         print("connected succesfully.")
+        return response
     
     else:
         print("connection failed.")
-
+        return None
 
 #getting photos ----------------------------------------------------------------------------
-def get_photos(url):
+def get_photos_data(url):
     response = send_req(url)
     if response:
-        pass
+        soup = BeautifulSoup(response.text, 'html.parser')
+        photos = soup.find_all('div',class_="Mhnzm")
+        photos_data = []
+        for photo in photos:
+            title = photo.find('a', class_="Prxeh")['title']
+            img_link = photo.find('img')['src']
+            img_link = URL + img_link
+            user_name = photo.find('span', class_="N25dY")['href']
+            data = {
+                'title' : title,
+                'img_link': img_link,
+                'user_name' :user_name
+            }
+            photos_data.append(data)
+        return photos_data
 
     else:
         return[]
     
-get_photos(URL)
+#save imge--------------------------------------------------------------------
+def save_img(content, path):
+    try:
+        with open(path , "wb" ) as img:       #wb ==> binary
+            img.write(content)
+    except Exception as e :
+        print (e)
+
+#download image----------------------------------------------------------------
+def download_img(title, img_url, folder):
+    response = send_req(img_url)
+    if response:
+        path = os.path.join(folder, title + ".jpg")
+        save_img(response.content, path)  #use content becuse it's photo and binarys
+    else:
+        return None
+    
+#create directory---------------------------------------------------------------
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    
+
+
+#test for running_________________________________________________________
+photos = get_photos_data(URL)
+for photo in photos:
+    title = photo['title']
+    title = title.replace(" ", "_")
+    photo_dir = os.path.join(BASE_DIR, title)  #make the direction
+    create_dir(photo_dir)
+    download_img(photo['img_link'], photo_dir)
